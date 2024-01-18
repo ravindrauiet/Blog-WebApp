647449d4fd93f6a3fcd8c5115bc73b09fabea496
Ravindra Nath Jha <ravindranathjha76@gmail.com>
2024-01-18 23:24:55 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO
+).then(() => {
+    console.log('MongoDb is connected...')
+}).catch((err) => {
+    console.error("Error connecting to MongoDB", err);
+});
 
 const app = express();
 
-app.listen(3000,()=>{
-    console.log('listening on 3000!');
+app.listen(3000, () => {
+  console.log("listening on 3000!");
 });
